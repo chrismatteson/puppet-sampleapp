6ef0d44ca9de026aba5e982b4303de2a61d7a0f7
Chris Matteson <chris.matteson@puppetlabs.com>
2015-07-25 23:46:36 +0000

    more testing changes

---
+++ spec/acceptance/acceptance.rb
@@ -0,0 +1,21 @@
+require 'spec_helper_acceptance'
+
+describe 'Acceptance test' do
+  describe 'running puppet code' do
+    it 'should work with no errors' do
+      pp = <<-EOS
+        class { 'sampleapp': deployref => 'master' }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+    end
+
+    describe 'should not return an error on curl' do
+      it 'should delete accounts' do
+        shell("curl localhost:8080/sample -f", :acceptable_exit_codes => 1)
+      end
+    end
+  end
+end
